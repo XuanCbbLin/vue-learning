937114d8a281206cf9c8d58a0149717d3c040b1e
xuan <xuancbblin@gmail.com>
2022-07-09 21:21:28 +0800

    feat : 1. init vue3 project  2. install windicss

---
+++ vue3-project/src/components/Computed.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="fullName = 'ssss xuan'">點我</button>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+
+const firstName = ref("John");
+const lastName = ref("Doe");
+
+const fullName = computed({
+  // getter
+  get() {
+    return firstName.value + " " + lastName.value;
+  },
+  // setter
+  set(newValue) {
+    // Note: we are using destructuring assignment syntax here.
+    [firstName.value, lastName.value] = newValue.split(" ");
+  },
+});
+</script>
+
+<style></style>
